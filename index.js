f4258c182b8c7eeb29b7397126a26dbb66e48dc5
Zach Frohardt <zfrohardt@gmail.com>
2021-04-06 10:27:25 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(arr) {
+    let obj = arr.find(callback);
 
+    if (obj !== undefined) {
+        return obj.year;
+    }
+}
+
+function callback(element, index, arr) {
+    return element.result === "W";
+}
